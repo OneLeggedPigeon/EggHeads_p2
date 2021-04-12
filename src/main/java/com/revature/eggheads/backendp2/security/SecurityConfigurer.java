a3a2cac3c0bcd7c30786a2fa276da78db61facd1
Brandon <brandon.a.pinkerton@gmail.com>
2021-04-12 17:40:55 -0500

    add password encoder to security config

--- src/main/java/com/revature/eggheads/backendp2/security/SecurityConfigurer.java
+++ src/main/java/com/revature/eggheads/backendp2/security/SecurityConfigurer.java
@@ -53,7 +53,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
