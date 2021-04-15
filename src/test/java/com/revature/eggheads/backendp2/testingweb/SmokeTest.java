c91f7388f2d15e8966731fec2e724691c29eae15
Nate Ford <nathaniel.ford@revature.net>
2021-04-15 15:39:30 -0500

    This is the test that caught the problem solved in the last commit

---
+++ src/test/java/com/revature/eggheads/backendp2/testingweb/SmokeTest.java
@@ -0,0 +1,40 @@
+package com.revature.eggheads.backendp2.testingweb;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.revature.eggheads.backendp2.controller.*;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private EggTemplateController eggTemplateController;
+
+    @Autowired
+    private EggImageController eggImageController;
+
+    @Autowired
+    private IncubatorController incubatorController;
+
+    @Autowired
+    private PetController petController;
+
+    @Autowired
+    private UserController userController;
+
+    /**
+     * Tests if all of the Controllers are formatted well enough be created
+     */
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(eggTemplateController).isNotNull();
+        assertThat(eggImageController).isNotNull();
+        assertThat(incubatorController).isNotNull();
+        assertThat(petController).isNotNull();
+        assertThat(userController).isNotNull();
+    }
+}
