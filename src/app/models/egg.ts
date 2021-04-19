ac253955ca5d9a2afc6a2656947966d2ad96e3dd
Nate Ford <nathaniel.ford@revature.net>
2021-04-19 09:53:24 -0500

    added properties to the egg model

--- src/app/models/egg.ts
+++ src/app/models/egg.ts
@@ -1,2 +1,15 @@
+import { Timestamp } from "rxjs";
+
 export class Egg {
+    id!:number;
+    isReady!:boolean;
+    timeCreated!:Date;
+    timeComplete!:Date;
+    startingSize!:number;
+    maxSize!:number;
+    currentSize!:number;
+    redValue!:number;
+    greenValue!:number;
+    blueValue!:number;
+    animalType!:string;
 }
