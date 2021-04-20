eb8a8e216b4272c45d9708451b1dec0f16126d51
Nate Ford <nathaniel.ford@revature.net>
2021-04-20 13:51:19 -0500

    removed old java code that got back in here somehow

--- src/main/java/com/revature/eggheads/frontendp2/exception/ParameterNotIncludedException.java
+++
@@ -1,7 +0,0 @@
-package com.revature.eggheads.frontendp2.exception;
-
-public class ParameterNotIncludedException extends Exception{
-    public ParameterNotIncludedException(String message) {
-        super(message);
-    }
-}
