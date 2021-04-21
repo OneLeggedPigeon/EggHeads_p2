c4b98a633dec521f73d02daad7e924ff0106835a
tkpanke <tpanke16@georgefox.edu>
2021-04-21 06:21:28 -0700

    building new template form

--- src/app/components/forms/new-template/new-template.component.ts
+++ src/app/components/forms/new-template/new-template.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EggTemplate } from 'src/app/models/egg-template';
+import { EggTemplateService } from 'src/app/services/egg-template.service';
 
 @Component({
   selector: 'app-new-template',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-template.component.css']
 })
 export class NewTemplateComponent implements OnInit {
+  id!: number;
+  name!: string;
+  incubationPeriod!: number;
+  size!: number;
+  redVal!: number;
+  greenVal!: number;
+  blueVal!: number;
 
-  constructor() { }
+  constructor(private templateService:EggTemplateService) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate() {
+    let template:EggTemplate = {
+      // hardcode in an id or nah? how to set?
+      id: this.id,
+      eggTemplateName: this.name,
+      incubationPeriod: this.incubationPeriod,
+      size: this.size,
+      redValue: this.redVal,
+      greenValue: this.greenVal,
+      blueValue: this.blueVal
+    }
+
+    this.templateService.addEggTemplate(template);
+    console.log("created, maybe");
   }
 
 }
