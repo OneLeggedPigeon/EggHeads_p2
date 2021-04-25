ee9f2e4fbc2dbb2a1fd4968a5c3613cd7699788b
Brandon <brandon.a.pinkerton@gmail.com>
2021-04-25 11:04:15 -0500

    nav changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { of } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,7 +12,39 @@ export class NavbarComponent implements OnInit {
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
   constructor(private userService: UserService) { }
 
+  fullNav:boolean = true;
+  username!:String | null;
+
+  menuItems = [
+    {
+      route: '/dashboard',
+      icon: 'home',
+      label: 'Dashboard'
+    },
+    {
+      route: '/incubator',
+      icon: 'inbox',
+      label: 'My Incubator'
+    },
+    {
+      route: '/pets',
+      icon: 'pets',
+      label: 'My Pets'
+    },
+    {
+      route: '/market',
+      icon: 'store',
+      label: 'Egg Market'
+    },
+    {
+      route: '/templates',
+      icon: 'add',
+      label: 'Create A Template'
+    }
+  ]
+
   ngOnInit(): void {
+    this.username = localStorage.getItem("username");
   }
 
   someMethod() {
@@ -19,6 +52,7 @@ export class NavbarComponent implements OnInit {
   }
 
   logout(){
+    alert("Goodbye, " + this.username);
     this.userService.logoutUser();
   }
 
